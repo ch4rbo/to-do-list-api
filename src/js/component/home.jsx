d41b4a2017cc73ccf0dd0d29c2df679df67729f0
ch4rbo <baueratctu@gmail.com>
2021-09-17 23:46:08 +0000

    comienzo ejercicio

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [tarea, setTarea] = useState("");
+	const [listaTareas, setListaTareas] = useState([]);
+	let tarea = "Primera tarea"
+	let tareas = []
+	tareas.push(tarea)
+	tareas.push(tarea)
+	tareas.push(tarea)
+	tareas.map(tarea => {
+
+		console.log(tarea);
+	});
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<h1>todos</h1>
+			<div>
+				<input
+					type="text"
+					className="form-control"
+					placeholder="Agregar tarea" />
+					onChange={e => setTarea(e.target.value)}
+					onKeyPress={}
+					value=[tarea]
+
+
+				<ul>
+				</ul>
+			</div>
 		</div>
 	);
 };

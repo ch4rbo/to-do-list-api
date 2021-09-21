e315208757497383ecb3a204095ad18ae5a31f36
ch4rbo <baueratctu@gmail.com>
2021-09-21 23:44:19 +0000

    falta darle estilo, funcionalidad lista

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,35 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
 	const [tarea, setTarea] = useState("");
 	const [listaTareas, setListaTareas] = useState([]);
-	let tarea = "Primera tarea"
-	let tareas = []
-	tareas.push(tarea)
-	tareas.push(tarea)
-	tareas.push(tarea)
-	tareas.map(tarea => {
 
-		console.log(tarea);
-	});
+	const handleKeyPress = event => {
+		if (event.key == "Enter") {
+			setListaTareas([...listaTareas, tarea]);
+			setTarea("");
+		}
+	};
+	const borrar = indice => {
+		let result = listaTareas.filter((tarea, index) => index !== indice);
+		setListaTareas(result);
+	};
+
 	return (
-		<div>
-			<h1>todos</h1>
+		<div className="text-center mt-5">
+			<h1>To do list</h1>
 			<div>
 				<input
 					type="text"
 					className="form-control"
-					placeholder="Agregar tarea" />
+					placeholder="Agregar tarea"
 					onChange={e => setTarea(e.target.value)}
-					onKeyPress={}
-					value=[tarea]
-
-
+					onKeyPress={f => handleKeyPress(f)}
+					value={tarea}
+				/>
 				<ul>
+					{listaTareas.map((item, index) => {
+						return (
+							<li key={index}>
+								{item}{" "}
+								<span onClick={() => borrar(index)}>
+									&#10006;
+								</span>{" "}
+							</li>
+						);
+					})}
 				</ul>
+			</div>
+			<div>
+				{" "}
+				<p>contador de tareas {listaTareas.length}</p>{" "}
 			</div>
 		</div>
 	);
 };
-
 export default Home;

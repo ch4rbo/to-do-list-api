a2afa3ec9ad4a839bd8f28581c33d108a0e3d989
ch4rbo <baueratctu@gmail.com>
2021-09-23 17:57:11 +0000

    gitpod is working ALELUYAA

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,38 +1,92 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
-	const [tarea, setTarea] = useState("");
-	const [listaTareas, setListaTareas] = useState([]);
+	const [task, setTask] = useState("");
+	const [taskList, setTaskList] = useState([]);
+	const urlApi = "https://assets.breatheco.de/apis/fake/todos/user/ch4rbo";
 
-	const handleKeyPress = event => {
-		if (event.key == "Enter") {
-			setListaTareas([...listaTareas, tarea]);
-			setTarea("");
+	useEffect(() => {
+		putTareas();
+	}, [taskList]);
+
+	useEffect(() => {
+		getTareas();
+	}, []);
+	const getTareas = () => {
+		fetch(urlApi, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(data => {
+				//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+				setTaskList(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+			})
+			.catch(error => {
+				//manejo de errores
+				alert(error);
+			});
+	};
+
+	const putTareas = () => {
+		fetch(urlApi, {
+			method: "PUT",
+			body: JSON.stringify(taskList),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(data => {
+				//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+				console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+			})
+			.catch(error => {
+				//manejo de errores
+				alert(error);
+			});
+	};
+
+	const agregarTarea = evento => {
+		if (evento.key == "Enter") {
+			let newTask = {
+				label: task,
+				done: false
+			};
+			setTaskList([...taskList, newTask]);
+			setTask("");
 		}
 	};
-	const borrar = indice => {
-		let result = listaTareas.filter((tarea, index) => index !== indice);
-		setListaTareas(result);
+	const borrarTarea = indice => {
+		let result = taskList.filter((task, index) => index !== indice);
+		setTaskList(result);
 	};
 
 	return (
-		<div className="container text-center mt-5">
-			<h1 id="titulo">To do list</h1>
-			<div>
+		<div className="superContenedor container d-flex justify-content-start flex-column align-items-center">
+			<h1>TODOS</h1>
+			<div className="contenedorTareas">
 				<input
 					type="text"
 					className="form-control"
 					placeholder="Agregar tarea"
-					onChange={e => setTarea(e.target.value)}
-					onKeyPress={f => handleKeyPress(f)}
-					value={tarea}
+					onChange={e => setTask(e.target.value)}
+					onKeyPress={f => agregarTarea(f)}
+					value={task}
 				/>
+
 				<ul>
-					{listaTareas.map((item, index) => {
+					{taskList.map((item, index) => {
 						return (
 							<li key={index}>
-								{item}{" "}
-								<span onClick={() => borrar(index)}>
+								{item.label}{" "}
+								<span onClick={() => borrarTarea(index)}>
 									&#10006;
 								</span>{" "}
 							</li>
@@ -41,10 +95,10 @@ const Home = () => {
 				</ul>
 			</div>
 			<div>
-				{" "}
-				<p>contador de tareas {listaTareas.length}</p>{" "}
+				<p>Task Counter {taskList.length}</p>
 			</div>
 		</div>
 	);
 };
+
 export default Home;
